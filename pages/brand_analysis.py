17b6b6cb0d8dcd9d57ffb8cad033d652cfb86370
Jaspreet Kaur <78718558+JaspreetKaur0410@users.noreply.github.com>
2023-11-30 18:31:41 -0500

    Update brand_analysis.py

--- pages/brand_analysis.py
+++ pages/brand_analysis.py
@@ -10,17 +10,21 @@ df_data_users=convert_to_pd(get_users_data_filters())
 # st.dataframe(df_data_users)
 # end - USER DATA
 
-# start - Number of products for brands
-get_count_products_by_brand=get_count_products_for_brand()
-df_get_count_prodcutts_by_brand=pd.DataFrame(get_count_products_by_brand)
-st.info('Top Brands based on highest number of products in the market')
-chart_count_products_by_brand=alt.Chart(df_get_count_prodcutts_by_brand).mark_bar(size=4).encode(
-        x='total_products_for_brand',
-        y='brand_name',
-        color='total_products_for_brand'
-        ).properties(width = 1900, height = 300)
-st.altair_chart(chart_count_products_by_brand, use_container_width=True)
-# end - Number of products for brands
+# start - Brands Used by customers - recommended by Social-Media & Influencers
+st.info('Top Brands Used by customers - recommended by Social-Media & Influencers')
+df_socialmedia_influencer_recomm=convert_to_pd(get_socialmedia_influencer_recomm())
+st.dataframe(df_socialmedia_influencer_recomm)
+
+base_chart = alt.Chart(df_socialmedia_influencer_recomm).mark_bar().encode(
+            y = alt.Y("brandName")
+        ).properties(
+            width=300,
+            height=200
+        )
+print(df_socialmedia_influencer_recomm['count_influencer_recommended'])
+base_chart.encode(x=alt.X('count_influencer_recommended')) | base_chart.encode(x=alt.X('count_social_media_recommended'))
+# st.altair_chart(base_chart)
+# end - Brands Used by customers - recommended by Social-Media & Influencers
 
 # start - highest-price-product for different brands
 df_get_highest_price_by_brand=convert_to_pd(get_highest_price_by_brand())
@@ -38,6 +42,18 @@ chart_count_people_by_brand=alt.Chart(df_people_count_by_brand).mark_bar(size=4)
         ).properties(width = 1800, height = 500)
 st.altair_chart(chart_count_people_by_brand, use_container_width=True)
 # end - how many people using different brands
+
+# start - Number of products for brands
+get_count_products_by_brand=get_count_products_for_brand()
+df_get_count_prodcutts_by_brand=pd.DataFrame(get_count_products_by_brand)
+st.info('Top Brands based on highest number of products in the market')
+chart_count_products_by_brand=alt.Chart(df_get_count_prodcutts_by_brand).mark_bar(size=4).encode(
+        x='total_products_for_brand',
+        y='brand_name',
+        color='total_products_for_brand'
+        ).properties(width = 1900, height = 300)
+st.altair_chart(chart_count_products_by_brand, use_container_width=True)
+# end - Number of products for brands
 
 # start - How many purchases from each brand from different stores
 st.info('Number of Purchases for different brands from different stores - Amazon has more hold than Nykaa & Local-Stores')
@@ -68,21 +84,7 @@ chart_recommended_count=alt.Chart(df_recommended_count).mark_bar(size=6).encode(
 st.altair_chart(chart_recommended_count)
 # end - How many people for each "recommended_by" recommeded the brand 
 
-# start - Brands Used by customers - recommended by Social-Media & Influencers
-st.info('Top Brands Used by customers - recommended by Social-Media & Influencers')
-df_socialmedia_influencer_recomm=convert_to_pd(get_socialmedia_influencer_recomm())
-st.dataframe(df_socialmedia_influencer_recomm)
 
-base_chart = alt.Chart(df_socialmedia_influencer_recomm).mark_bar().encode(
-            y = alt.Y("brandName")
-        ).properties(
-            width=300,
-            height=200
-        )
-print(df_socialmedia_influencer_recomm['count_influencer_recommended'])
-base_chart.encode(x=alt.X('count_influencer_recommended')) | base_chart.encode(x=alt.X('count_social_media_recommended'))
-# st.altair_chart(base_chart)
-# end - Brands Used by customers - recommended by Social-Media & Influencers
 
 
 
